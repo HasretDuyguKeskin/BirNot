87205f30868361fe5da91d21dcffb5b4d83e8ff8
hasret keskin <hasret_1989_@hotmail.com>
2021-04-07 10:50:43 +0300

    Add project files.

---
+++ BirNotAPI/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BirNotAPI.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "deger1", "deger2" };
+
+          // startup.cs  app.UseCors(CorsOptions.AllowAll);//Windows.Owin kurulduktan sonra eklenir.
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "deger";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
