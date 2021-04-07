2c4380687944c6797270b1fe30d1ca563cbc34ce
hasret keskin <hasret_1989_@hotmail.com>
2021-04-07 12:48:11 +0300

    notApi

--- BirNotAPI/Controllers/ValuesController.cs
+++ BirNotAPI/Controllers/ValuesController.cs
@@ -15,6 +15,7 @@ namespace BirNotAPI.Controllers
         {
             return new string[] { "deger1", "deger2" };
 
+            // startup.cs - app.UseCors(CorsOptions.AllowAll);   Windows.Owin kurulduktan sonra eklenir.
         }
 
         // GET api/values/5
---
+++ BirNotAPI/Extensions/NotExtensions.cs
@@ -0,0 +1,23 @@
+﻿using BirNotAPI.DTOs;
+using BirNotAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BirNotAPI.Extensions
+{
+    public static class NotExtensions //Kod tekrarı önlemek için class oluşturduk
+    {
+        public static GetNotDto ToGetNotDTO(this Not not)
+        {
+            return new GetNotDto
+            {
+                Id = not.Id,
+                Baslik = not.Baslik,
+                Icerik = not.Icerik,
+                SonDegistirilme = not.SonDegistirilme
+            };
+        }
+    }
+}
